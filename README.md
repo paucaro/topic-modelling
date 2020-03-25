f01c3c1ea95bcae25fa0f0bef94649d4fdf1e8a1
Paula Asto <carolinaasto@gmail.com>
2020-03-25 18:04:25 -0500

    nmf and lda Comparison and update readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# topic-modelling
-Proposal to topic modelling
+# Topic Modelling
+
+Latent Dirichlet Allocation (LDA) is a algorithms used to discover the topics that are present in a corpus. Python with some libraries could be used to to find topics in text.
+
+## How to install
+Install pandas to load csv file:
+``` 
+pip install pandas
+pip install gensim
+pip install numpy
+pip install nltk
+```
+## How to use
+To run topic-v1:
+```
+python topic-v1.py
+```
+To run topic-v2:
+```
+python topic-v2.py
+```
+To run topic-v3:
+```
+python topic-v3.py
+```
+# Implementation
+## Topic v1 and Topic v2
+### DATA Pre-processing
+- Tokenization: split text into sentences and the sentences into words. Lowercase the words and remove punctuation
+- Stopwords removed
+- Word are lemmatized - words in thrid person are changed to first person and verb in past and future tenses are changed into present
+- Words are stemmed - words are reduced to their root form
+
+Using gensim library to get topic modelling.
+
+## Topic v3
+NMF and LDA comparison with sklearn library
+
--- topic-v2.py
+++ topic-v2.py
@@ -1,13 +1,10 @@
-# packages to store and manipulate data
 import pandas as pd
-# importing required libraries 
 import nltk
 nltk.download('stopwords')
 nltk.download('wordnet')
 from nltk.corpus import stopwords 
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
-# Importing Gensim
 import gensim
 from gensim import corpora
 
@@ -35,12 +32,12 @@ dictionary = corpora.Dictionary(doc_clean)
 doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
 Lda = gensim.models.ldamodel.LdaModel
 ldamodel = Lda(doc_term_matrix, num_topics=7, id2word = dictionary, passes=50)
-#print(ldamodel.print_topics(num_topics=7, num_words=4))
+print(ldamodel.print_topics(num_topics=7, num_words=3))
 
 
 ##### test
-unseen_document = 'hello, how are you?'
-bow_vector = dictionary.doc2bow(clean(unseen_document).split())
+#unseen_document = 'hello, how are you?'
+#bow_vector = dictionary.doc2bow(clean(unseen_document).split())
 
-for index, score in sorted(ldamodel[bow_vector], key=lambda tup: -1*tup[1]):
-    print("Score: {}\t Topic: {}".format(score, ldamodel.print_topic(index, 4)))
+#for index, score in sorted(ldamodel[bow_vector], key=lambda tup: -1*tup[1]):
+#    print("Score: {}\t Topic: {}".format(score, ldamodel.print_topic(index, 4)))
